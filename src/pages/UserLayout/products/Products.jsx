52d2224107b27f7aec244edcdb3e1356dddd804e
karim ali <karimalielsayed88@gmail.com>
2024-10-11 23:29:18 +0300

    unique db and connect links

--- src/pages/UserLayout/products/Products.jsx
+++ src/pages/UserLayout/products/Products.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FiHeart } from "react-icons/fi";
-
+import { Link } from "react-router-dom";
 import {
   Card,
   CardHeader,
@@ -12,7 +12,7 @@ import {
 } from "@material-tailwind/react";
 
 const Products = ({
-  product: { description, image, offers, price, title },
+  product: { description, image, offers, price, title,id },
 }) => {
 
 
@@ -69,7 +69,7 @@ const Products = ({
           <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
         </IconButton>
         <Button size="lg" className="bg-deep-orange-600">
-          Details
+          <Link to={`/products/${id}`}>Details</Link>
         </Button>
       </CardFooter>
     </Card>
